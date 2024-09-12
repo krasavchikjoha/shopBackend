40b3536c8ee629480a7bd1e21ce789727f26e17d
krasavchikjoha <joha.uzbsila2002@gmail.com>
2024-09-12 20:02:42 +0500

    cors

--- functions/api.js
+++ functions/api.js
@@ -3,8 +3,10 @@ import ServerlessHttp from "serverless-http";
 import { configDotenv } from "dotenv";
 import mongoose from "mongoose";
 import { userRoutes } from "../Routes/userRoutes.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Welcome to Server");
